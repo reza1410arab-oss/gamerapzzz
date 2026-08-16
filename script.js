27b95614dcb59ffc63d087f1305a715943188816
reza1410arab-oss <reza1410arab@gmail.com>
2026-08-16 14:32:26 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,673 @@
+
+const dataGame = {
+
+    1:{
+        nama:"MAHJONG WAYS 1",
+        saran:[
+`Dicoba bermain di game ini bos, lagi gacor bosku di PG SOFT - Mahjong WAYS
+
+🔥 10x SPIN OTOMATIS TURBO ON
+🔥 8x SPIN MANUAL TURBO OFF
+🔥 20x SPIN OTOMATIS TURBO ON
+🔥 15x SPIN MANUAL TURBO OFF`
+        ],
+        pola:[
+            "Pola santai: 10 putaran biasa → istirahat sebentar → lanjut 10 putaran."
+        ]
+    },
+
+    2:{
+        nama:"GAME 2",
+        saran:[
+`Dicoba bermain di game ini bos, lagi gacor bosku di PRAGMATIC PLAY - Gates Of Gatot Kaca Super Scatter
+
+✅❎❎ 18x Manual DC OFF
+❎✅❎ 8x Quick DC ON
+❎✅✅ 13x Manual DC OFF
+✅❎✅ 10x Quick DC ON
+❎✅❎ 25x Quick DC ON`
+        ],
+        pola:[
+            "Pola ringan: 5 putaran → 5 putaran → jeda singkat."
+        ]
+    },
+
+    3:{
+        nama:"GAME 3",
+        saran:[
+`Dicoba bermain di game ini bos, lagi gacor bosku di PG SOFT - Wild Bandito
+
+🔥 15x SPIN OTOMATIS TURBO ON
+🔥 25x SPIN MANUAL TURBO OFF
+🔥 10x SPIN OTOMATIS TURBO ON
+🔥 5x SPIN MANUAL TURBO OFF`
+        ],
+        pola:[
+            "Pola santai: 10 putaran biasa → istirahat sebentar → lanjut 10 putaran."
+        ]
+    },
+
+    4:{
+        nama:"GAME 4",
+        saran:[
+`Dicoba bermain di game ini bos, lagi gacor bosku di PRAGMATIC PLAY - Sweet Bonanza SUPER SCATTER
+
+✅❎❎ 22x Manual DC OFF
+❎✅❎ 9x Quick DC ON
+❎✅✅ 15x Manual DC OFF
+✅❎✅ 10x Quick DC ON
+❎✅❎ 23x Quick DC ON`
+        ],
+        pola:[
+            "Pola ringan: 5 putaran → 5 putaran → jeda singkat."
+        ]
+    },
+
+    5:{
+        nama:"GAME 5",
+        saran:[
+`Dicoba bermain di game ini bos, lagi gacor bosku di PG SOFT - Wild Bounty SHOWDOWN
+
+🔥 10x SPIN OTOMATIS TURBO ON
+🔥 8x SPIN MANUAL TURBO OFF
+🔥 20x SPIN OTOMATIS TURBO ON
+🔥 15x SPIN MANUAL TURBO OFF`
+        ],
+        pola:[
+            "Pola santai: 10 putaran biasa → istirahat sebentar → lanjut 10 putaran."
+        ]
+    },
+
+    6:{
+        nama:"GAME 6",
+        saran:[
+`Dicoba bermain di game ini bos, lagi gacor bosku di PRAGMATIC PLAY - GATES of OLYMPUS
+
+✅❎❎ 11x Manual DC OFF
+❎✅❎ 21x Quick DC ON
+❎✅✅ 17x Manual DC OFF
+✅❎✅ 7x Quick DC ON
+❎✅❎ 15x Quick DC ON`
+        ],
+        pola:[
+            "Pola ringan: 5 putaran → 5 putaran → jeda singkat."
+        ]
+    },
+
+    7:{
+        nama:"GAME 7",
+        saran:[
+`Dicoba bermain di game ini bos, lagi gacor bosku di PG SOFT - Mahjong Ways 2
+
+🔥 10x SPIN OTOMATIS TURBO ON
+🔥 5x SPIN MANUAL TURBO OFF
+🔥 20x SPIN OTOMATIS TURBO ON
+🔥 17x SPIN MANUAL TURBO OFF`
+        ],
+        pola:[
+            "Pola santai: 10 putaran biasa → istirahat sebentar → lanjut 10 putaran."
+        ]
+    },
+
+    8:{
+        nama:"GAME 8",
+        saran:[
+`Dicoba bermain di game ini bos, lagi gacor bosku di PRAGMATIC PLAY - Starlight Princess SUPER SCATTER
+
+✅❎❎ 18x Manual DC OFF
+❎✅❎ 8x Quick DC ON
+❎✅✅ 13x Manual DC OFF
+✅❎✅ 10x Quick DC ON
+❎✅❎ 25x Quick DC ON`
+        ],
+        pola:[
+            "Pola ringan: 5 putaran → 5 putaran → jeda singkat."
+        ]
+    },
+
+    9:{
+        nama:"GAME 9",
+        saran:[
+`Dicoba bermain di game ini bos, lagi gacor bosku di PG SOFT - Caishen WINS
+
+🔥 10x SPIN OTOMATIS TURBO ON
+🔥 17x SPIN MANUAL TURBO OFF
+🔥 10x SPIN OTOMATIS TURBO ON
+🔥 7x SPIN MANUAL TURBO OFF`
+        ],
+        pola:[
+            "Pola santai: 10 putaran biasa → istirahat sebentar → lanjut 10 putaran."
+        ]
+    },
+
+    10:{
+        nama:"GAME 10",
+        saran:[
+`Dicoba bermain di game ini bos, lagi gacor bosku di PRAGMATIC PLAY - Dragon POTS
+
+✅❎❎ 17x Manual DC OFF
+❎✅❎ 8x Quick DC ON
+❎✅✅ 12x Manual DC OFF
+✅❎✅ 10x Quick DC ON
+❎✅❎ 30x Quick DC ON`
+        ],
+        pola:[
+            "Pola ringan: 5 putaran → 5 putaran → jeda singkat."
+        ]
+    },
+
+    11:{
+        nama:"GAME 11",
+        saran:[
+`Dicoba bermain di game ini bos, lagi gacor bosku di PG SOFT - Mafia Mayhem
+
+🔥 20x SPIN OTOMATIS TURBO ON
+🔥 12x SPIN MANUAL TURBO OFF
+🔥 10x SPIN OTOMATIS TURBO ON
+🔥 8x SPIN MANUAL TURBO OFF`
+        ],
+        pola:[
+            "Pola santai: 10 putaran biasa → istirahat sebentar → lanjut 10 putaran."
+        ]
+    },
+
+    12:{
+        nama:"GAME 12",
+        saran:[
+`Dicoba bermain di game ini bos, lagi gacor bosku di PRAGMATIC PLAY - Great Ghosts
+
+✅❎❎ 15x Manual DC OFF
+❎✅❎ 10x Quick DC ON
+❎✅✅ 8x Manual DC OFF
+✅❎✅ 20x Quick DC ON
+❎✅❎ 15x Quick DC ON`
+        ],
+        pola:[
+            "Pola ringan: 5 putaran → 5 putaran → jeda singkat."
+        ]
+    },
+
+    13:{
+        nama:"GAME 13",
+        saran:[
+`Dicoba bermain di game ini bos, lagi gacor bosku di PG SOFT - Dragon Hatch
+
+🔥 10x SPIN OTOMATIS TURBO ON
+🔥 5x SPIN MANUAL TURBO OFF
+🔥 20x SPIN OTOMATIS TURBO ON
+🔥 7x SPIN MANUAL TURBO OFF`
+        ],
+        pola:[
+            "Pola santai: 10 putaran biasa → istirahat sebentar → lanjut 10 putaran."
+        ]
+    },
+
+    14:{
+        nama:"GAME 14",
+        saran:[
+`Dicoba bermain di game ini bos, lagi gacor bosku di PRAGMATIC PLAY - Aztec Gems
+
+✅❎❎ 16x Manual DC OFF
+❎✅❎ 9x Quick DC ON
+❎✅✅ 15x Manual DC OFF
+✅❎✅ 8x Quick DC ON
+❎✅❎ 30x Quick DC ON`
+        ],
+        pola:[
+            "Pola ringan: 5 putaran → 5 putaran → jeda singkat."
+        ]
+    },
+    15:{
+        nama:"GAME 15",
+        saran:[
+`Dicoba bermain di game ini bos, lagi gacor bosku di PG SOFT - Pinata WINS
+
+🔥 30x SPIN MANUAL TURBO OFF
+🔥 15x SPIN OTOMATIS TURBO ON
+🔥 7x SPIN MANUAL TURBO OFF`
+        ],
+        pola:[
+            "Pola santai: 10 putaran biasa → istirahat sebentar → lanjut 10 putaran."
+        ]
+    },
+    16:{
+        nama:"GAME 16",
+        saran:[
+`Dicoba bermain di game ini bos, lagi gacor bosku di PRAGMATIC PLAY - Wisdom OF ANTHENA 1000
+
+✅❎❎ 10x Manual DC OFF
+❎✅❎ 9x Quick DC ON
+❎✅✅ 18x Manual DC OFF
+✅❎✅ 15x Quick DC ON
+❎✅❎ 20x Quick DC ON`
+        ],
+        pola:[
+            "Pola ringan: 5 putaran → 5 putaran → jeda singkat."
+        ]
+    },
+    17:{
+        nama:"GAME 17",
+        saran:[
+`Dicoba bermain di game ini bos, lagi gacor bosku di PG SOFT - Yakuza Honor
+
+🔥 30x SPIN MANUAL TURBO OFF
+🔥 15x SPIN OTOMATIS TURBO ON
+🔥 7x SPIN MANUAL TURBO OFF`
+        ],
+        pola:[
+            "Pola tampilan: bagian 1 → bagian 2 → jeda."
+        ]
+    },
+    18:{
+        nama:"GAME 18",
+        saran:[
+`GAME 18 - SARAN SEMENTARA
+
+🎮 Pilihan game 18
+🎮 Lihat fitur game
+🎮 Ganti teks ini nanti`
+        ],
+        pola:[
+            "Pola tampilan: bagian 1 → bagian 2 → lanjut."
+        ]
+    },
+
+    19:{
+        nama:"GAME 19",
+        saran:[
+`GAME 19 - SARAN SEMENTARA
+
+🎮 Pilihan game 19
+🎮 Lihat fitur game
+🎮 Ganti teks ini nanti`
+        ],
+        pola:[
+            "Pola tampilan: bagian 1 → bagian 2 → jeda."
+        ]
+    },
+
+    20:{
+        nama:"GAME 20",
+        saran:[
+`GAME 20 - SARAN SEMENTARA
+
+🎮 Pilihan game 20
+🎮 Lihat fitur game
+🎮 Ganti teks ini nanti`
+        ],
+        pola:[
+            "Pola tampilan: bagian 1 → bagian 2 → lanjut."
+        ]
+    },
+
+    21:{
+        nama:"GAME 21",
+        saran:[
+`GAME 21 - SARAN SEMENTARA
+
+🎮 Pilihan game 21
+🎮 Lihat fitur game
+🎮 Ganti teks ini nanti`
+        ],
+        pola:[
+            "Pola tampilan: bagian 1 → bagian 2 → jeda."
+        ]
+    },
+
+    22:{
+        nama:"GAME 22",
+        saran:[
+`GAME 22 - SARAN SEMENTARA
+
+🎮 Pilihan game 22
+🎮 Lihat fitur game
+🎮 Ganti teks ini nanti`
+        ],
+        pola:[
+            "Pola tampilan: bagian 1 → bagian 2 → lanjut."
+        ]
+    },
+
+    23:{
+        nama:"GAME 23",
+        saran:[
+`GAME 23 - SARAN SEMENTARA
+
+🎮 Pilihan game 23
+🎮 Lihat fitur game
+🎮 Ganti teks ini nanti`
+        ],
+        pola:[
+            "Pola tampilan: bagian 1 → bagian 2 → jeda."
+        ]
+    },
+
+    24:{
+        nama:"GAME 24",
+        saran:[
+`GAME 24 - SARAN SEMENTARA
+
+🎮 Pilihan game 24
+🎮 Lihat fitur game
+🎮 Ganti teks ini nanti`
+        ],
+        pola:[
+            "Pola tampilan: bagian 1 → bagian 2 → lanjut."
+        ]
+    },
+
+    25:{
+        nama:"GAME 25",
+        saran:[
+`GAME 25 - SARAN SEMENTARA
+
+🎮 Pilihan game 25
+🎮 Lihat fitur game
+🎮 Ganti teks ini nanti`
+        ],
+        pola:[
+            "Pola tampilan: bagian 1 → bagian 2 → jeda."
+        ]
+    },
+
+    26:{
+        nama:"GAME 26",
+        saran:[
+`GAME 26 - SARAN SEMENTARA
+
+🎮 Pilihan game 26
+🎮 Lihat fitur game
+🎮 Ganti teks ini nanti`
+        ],
+        pola:[
+            "Pola tampilan: bagian 1 → bagian 2 → lanjut."
+        ]
+    },
+
+    27:{
+        nama:"GAME 27",
+        saran:[
+`GAME 27 - SARAN SEMENTARA
+
+🎮 Pilihan game 27
+🎮 Lihat fitur game
+🎮 Ganti teks ini nanti`
+        ],
+        pola:[
+            "Pola tampilan: bagian 1 → bagian 2 → jeda."
+        ]
+    },
+
+    28:{
+        nama:"GAME 28",
+        saran:[
+`GAME 28 - SARAN SEMENTARA
+
+🎮 Pilihan game 28
+🎮 Lihat fitur game
+🎮 Ganti teks ini nanti`
+        ],
+        pola:[
+            "Pola tampilan: bagian 1 → bagian 2 → lanjut."
+        ]
+    },
+
+    29:{
+        nama:"GAME 29",
+        saran:[
+`GAME 29 - SARAN SEMENTARA
+
+🎮 Pilihan game 29
+🎮 Lihat fitur game
+🎮 Ganti teks ini nanti`
+        ],
+        pola:[
+            "Pola tampilan: bagian 1 → bagian 2 → jeda."
+        ]
+    },
+
+    30:{
+        nama:"GAME 30",
+        saran:[
+`GAME 30 - SARAN SEMENTARA
+
+🎮 Pilihan game 30
+🎮 Lihat fitur game
+🎮 Ganti teks ini nanti`
+        ],
+        pola:[
+            "Pola tampilan: bagian 1 → bagian 2 → lanjut."
+        ]
+    },
+
+    31:{
+        nama:"GAME 31",
+        saran:[
+`GAME 31 - SARAN SEMENTARA
+
+🎮 Pilihan game 31
+🎮 Lihat fitur game
+🎮 Ganti teks ini nanti`
+        ],
+        pola:[
+            "Pola tampilan: bagian 1 → bagian 2 → jeda."
+        ]
+    },
+
+    32:{
+        nama:"GAME 32",
+        saran:[
+`GAME 32 - SARAN SEMENTARA
+
+🎮 Pilihan game 32
+🎮 Lihat fitur game
+🎮 Ganti teks ini nanti`
+        ],
+        pola:[
+            "Pola tampilan: bagian 1 → bagian 2 → lanjut."
+        ]
+    },
+
+    33:{
+        nama:"GAME 33",
+        saran:[
+`GAME 33 - SARAN SEMENTARA
+
+🎮 Pilihan game 33
+🎮 Lihat fitur game
+🎮 Ganti teks ini nanti`
+        ],
+        pola:[
+            "Pola tampilan: bagian 1 → bagian 2 → jeda."
+        ]
+    },
+
+    34:{
+        nama:"GAME 34",
+        saran:[
+`GAME 34 - SARAN SEMENTARA
+
+🎮 Pilihan game 34
+🎮 Lihat fitur game
+🎮 Ganti teks ini nanti`
+        ],
+        pola:[
+            "Pola tampilan: bagian 1 → bagian 2 → lanjut."
+        ]
+    },
+
+    35:{
+        nama:"GAME 35",
+        saran:[
+`GAME 35 - SARAN SEMENTARA
+
+🎮 Pilihan game 35
+🎮 Lihat fitur game
+🎮 Ganti teks ini nanti`
+        ],
+        pola:[
+            "Pola tampilan: bagian 1 → bagian 2 → jeda."
+        ]
+    },
+
+    36:{
+        nama:"GAME 36",
+        saran:[
+`GAME 36 - SARAN SEMENTARA
+
+🎮 Pilihan game 36
+🎮 Lihat fitur game
+🎮 Ganti teks ini nanti`
+        ],
+        pola:[
+            "Pola tampilan: bagian 1 → bagian 2 → lanjut."
+        ]
+    }
+
+};
+
+
+let hasilCopy = "";
+
+
+function ambilAcak(data){
+
+    return data[Math.floor(Math.random()*data.length)];
+
+}
+
+
+function saranGame(id){
+
+    const game = dataGame[id];
+
+    if(!game) return;
+
+    hasilCopy = ambilAcak(game.saran);
+
+    copyTeks();
+
+}
+
+
+async function copyTeks(){
+
+    try{
+
+        await navigator.clipboard.writeText(hasilCopy);
+
+    }
+
+    catch(error){
+
+        console.log("Copy teks gagal");
+
+    }
+
+}
+
+
+async function copyGameImage(id){
+
+    const img = document.querySelector(
+        ".game-box:nth-child("+id+") img"
+    );
+
+    if(!img){
+
+        console.log("Foto tidak ditemukan");
+
+        return;
+
+    }
+
+    try{
+
+        const canvas = document.createElement("canvas");
+
+        const ctx = canvas.getContext("2d");
+
+        const image = new Image();
+
+        image.src = img.src;
+
+        image.onload = async function(){
+
+            canvas.width = image.naturalWidth;
+
+            canvas.height = image.naturalHeight;
+
+            ctx.drawImage(
+                image,
+                0,
+                0
+            );
+
+            canvas.toBlob(async function(blob){
+
+                await navigator.clipboard.write([
+
+                    new ClipboardItem({
+
+                        "image/png":blob
+
+                    })
+
+                ]);
+
+            },"image/png");
+
+        };
+
+    }
+
+    catch(error){
+
+        console.log("Copy gambar gagal",error);
+
+    }
+
+}
+
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+    const container = document.getElementById("gameContainer");
+
+    if(!container) return;
+
+    for(let i=1;i<=36;i++){
+
+        container.innerHTML += `
+
+        <article class="game-box">
+
+            <div class="image-wrapper">
+
+                <img src="img/game${i}.jpg">
+
+            </div>
+
+            <div class="button-group">
+
+                <button class="btn btn-saran" onclick="saranGame(${i})">
+
+                    🎯 SARAN & POLA
+
+                </button>
+
+                <button class="btn btn-pola" onclick="copyGameImage(${i})">
+
+                    🔥 COPY GAME
+
+                </button>
+
+            </div>
+
+        </article>
+
+        `;
+
+    }
+
+});
